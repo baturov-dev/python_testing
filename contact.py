80757e84f2cb8e26ad477bfed99c28c4be7d3b9c
Andrey Baturov <butanolium@rambler.ru>
2018-03-07 14:34:12 +0300

    Работа с вспомогательным классом для контактов

---
+++ contact.py
@@ -0,0 +1,25 @@
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address,
+                 home_tel, mobile_tel, work_tel, fax, email, email2, email3, homepage, byear, ayear, address2,
+                 notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_tel = home_tel
+        self.mobile_tel = mobile_tel
+        self.work_tel = work_tel
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.byear = byear
+        self.ayear = ayear
+        self.address2 = address2
+        self.notes = notes
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import  unittest
+from contact import Contact
 
 def is_alert_present(wd):
     try:
@@ -19,87 +20,87 @@ class test_add_contact(unittest.TestCase):
         wd.get("http://localhost/addressbook/index.php")
 
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
 
-    def create_contact(self, wd):
+    def create_contact(self, wd, group):
         # init contact creation
         wd.find_element_by_link_text("add new").click()
         # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Petya")
+        wd.find_element_by_name("firstname").send_keys(group.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("Viktorovich")
+        wd.find_element_by_name("middlename").send_keys(group.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Volkov")
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("Batya")
+        wd.find_element_by_name("nickname").send_keys(group.nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("Nope")
+        wd.find_element_by_name("title").send_keys(group.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("Home")
+        wd.find_element_by_name("company").send_keys(group.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Moscow")
+        wd.find_element_by_name("address").send_keys(group.address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("1")
+        wd.find_element_by_name("home").send_keys(group.home_tel)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("2")
+        wd.find_element_by_name("mobile").send_keys(group.mobile_tel)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("3")
+        wd.find_element_by_name("work").send_keys(group.work_tel)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys("4")
+        wd.find_element_by_name("fax").send_keys(group.fax)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("yep@rambler.ru")
+        wd.find_element_by_name("email").send_keys(group.email)
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys("yep2@rambler.ru")
+        wd.find_element_by_name("email2").send_keys(group.email2)
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys("yep3@rambler.ru")
+        wd.find_element_by_name("email3").send_keys(group.email3)
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys("batya.ru")
+        wd.find_element_by_name("homepage").send_keys(group.homepage)
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[5]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[5]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1989")
+        wd.find_element_by_name("byear").send_keys(group.byear)
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[5]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[5]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[5]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[5]").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys("2009")
+        wd.find_element_by_name("ayear").send_keys(group.ayear)
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("Nope")
+        wd.find_element_by_name("address2").send_keys(group.address2)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("Nope")
+        wd.find_element_by_name("notes").send_keys(group.notes)
         # enter new contact
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
@@ -111,8 +112,11 @@ class test_add_contact(unittest.TestCase):
     def test_test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
-        self.create_contact(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_contact(wd, Contact(firstname="Petya", middlename="Viktorovich", lastname="Volkov", nickname="Batya",
+                            title="Nope", company="Home", address="Moscow", home_tel="1", mobile_tel="2", work_tel="3",
+                            fax="4", email="yep@rambler.ru", email2="yep2@rambler.ru", email3="yep3@rambler.ru",
+                            homepage="batya.ru", byear="1989", ayear="2009", address2="Nope", notes="Nope"))
         self.logout(wd)
 
 
